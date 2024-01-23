3f7da5f78ed735f00805931eba2984d55bd69268
Yogesh Rokhade <yogesh@mbp-von-yogesh.localdomain>
2024-01-23 23:35:19 +0100

    new trained model added

--- src/test.py
+++ src/test.py
@@ -3,10 +3,10 @@ import os
 
 import pytesseract
 
-testimage_dir = 'tesstrain/data/soi-test-data'
-imagename = f'{testimage_dir}/testpage.tif'
-outputfile = f'{testimage_dir}/ocr.txt'
-trained_model = 'tesstrain/data/soi'
+#testimage_dir = 'tesstrain/data/soi-test-data'
+#imagename = f'{testimage_dir}/testpage.tif'
+#outputfile = f'{testimage_dir}/ocr.txt'
+#trained_model = 'tesstrain/data/soi'
 
 # Set the path to the tessdata directory
 # tessdata_directory = '/opt/homebrew/Cellar/tesseract/5.3.3/share/tessdata'
@@ -23,12 +23,14 @@ trained_model = 'tesstrain/data/soi'
 #     '--oem', '0'
 # ])
 
-trained_data = "/opt/homebrew/Cellar/tesseract/5.3.3/share/tessdata/soi.traineddata"
+#trained_data = "/opt/homebrew/Cellar/tesseract/5.3.3/share/tessdata/soi.traineddata"
 # Set the path to the Tesseract executable (change this to your Tesseract installation path)
 # pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/Cellar/tesseract/5.3.3/bin/'
 
 # Specify the OCR Engine Mode (OEM) and Language
-custom_config = r'--oem 1 --psm 6 -l eng+soi'
+custom_config = r'--oem 1 --psm 6 -l soi'
+
+imagename = "testdata/SOI_test1.png"
 
 text = pytesseract.image_to_string(imagename, config=custom_config)
 
